348189df85ae7c16c1c7fbfd4aada6491e458a1f
gisellec <giselle.cduran@hotmail.com>
2023-12-05 13:07:57 +0100

    added guard for exit

--- src/app/website/pages/register/register.component.ts
+++ src/app/website/pages/register/register.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { OnExit } from "src/app/guards/exit.guard";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+	selector: "app-register",
+	templateUrl: "./register.component.html",
+	styleUrls: ["./register.component.scss"],
 })
-export class RegisterComponent {
-
+export class RegisterComponent implements OnExit {
+	onExit() {
+		const rta = confirm("Logica desde comp, estas seguro salir?");
+		return rta;
+	}
 }
